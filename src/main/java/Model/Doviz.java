f8fab1bdf900d6e6c67662d9702b8376a0e7e389
Burak <brkktby@gmail.com>
2017-05-18 10:09:48 +0300

    Model değişkenleri eklendi.

--- src/main/java/Model/Doviz.java
+++ src/main/java/Model/Doviz.java
@@ -5,6 +5,33 @@ import java.io.Serializable;
 /**
  * Created by hasanburakkutbay on 18.05.2017.
  */
-public class Doviz implements Serializable{
+public class Doviz implements Serializable {
 
+    private String dolarAlisFiyat;
+    private String dolarSatisFiyat;
+    private String guncellemeZamani;
+
+    public String getDolarAlisFiyat() {
+        return dolarAlisFiyat;
+    }
+
+    public void setDolarAlisFiyat(String dolarAlisFiyat) {
+        this.dolarAlisFiyat = dolarAlisFiyat;
+    }
+
+    public String getDolarSatisFiyat() {
+        return dolarSatisFiyat;
+    }
+
+    public void setDolarSatisFiyat(String dolarSatisFiyat) {
+        this.dolarSatisFiyat = dolarSatisFiyat;
+    }
+
+    public String getGuncellemeZamani() {
+        return guncellemeZamani;
+    }
+
+    public void setGuncellemeZamani(String guncellemeZamani) {
+        this.guncellemeZamani = guncellemeZamani;
+    }
 }
