fb835f55b10b5de2a837760d332842c90b80f24d
Burak <brkktby@gmail.com>
2017-05-18 23:52:27 +0300

    Doviz model ekleme

--- src/main/java/model/Doviz.java
+++ src/main/java/model/Doviz.java
@@ -9,7 +9,10 @@ public class Doviz implements Serializable {
 
     private String dolarAlisFiyat;
     private String dolarSatisFiyat;
+    private String euroAlisFiyat;
+    private String euroSatisFiyat;
     private String guncellemeZamani;
+    private String mesaj;
 
     public String getDolarAlisFiyat() {
         return dolarAlisFiyat;
@@ -33,5 +36,29 @@ public class Doviz implements Serializable {
 
     public void setGuncellemeZamani(String guncellemeZamani) {
         this.guncellemeZamani = guncellemeZamani;
+    }
+
+    public String getMesaj() {
+        return mesaj;
+    }
+
+    public void setMesaj(String mesaj) {
+        this.mesaj = mesaj;
+    }
+
+    public String getEuroAlisFiyat() {
+        return euroAlisFiyat;
+    }
+
+    public void setEuroAlisFiyat(String euroAlisFiyat) {
+        this.euroAlisFiyat = euroAlisFiyat;
+    }
+
+    public String getEuroSatisFiyat() {
+        return euroSatisFiyat;
+    }
+
+    public void setEuroSatisFiyat(String euroSatisFiyat) {
+        this.euroSatisFiyat = euroSatisFiyat;
     }
 }
