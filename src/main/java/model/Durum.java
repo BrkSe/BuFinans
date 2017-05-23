7ab3dc2b35a26821b8ea95bd0f1cfad4eda9a739
Burak <brkktby@gmail.com>
2017-05-23 13:38:28 +0300

    Verilerin ne gibi değişiklik gösterdiğini anlamak için kullanılması gereken işaretlerin olacağı Model

---
+++ src/main/java/model/Durum.java
@@ -0,0 +1,62 @@
+package model;
+
+/**
+ * Created by hasanburakkutbay on 23.05.2017.
+ */
+public class Durum {
+
+    private String dolarDoviz;
+    private String euroDoviz;
+    private String ceyrekAltin;
+    private String yarimAltin;
+    private String tamAltin;
+    private String cumhuriyetAltin;
+
+    public String getDolarDoviz() {
+        return dolarDoviz;
+    }
+
+    public void setDolarDoviz(String dolarDoviz) {
+        this.dolarDoviz = dolarDoviz;
+    }
+
+    public String getEuroDoviz() {
+        return euroDoviz;
+    }
+
+    public void setEuroDoviz(String euroDoviz) {
+        this.euroDoviz = euroDoviz;
+    }
+
+    public String getCeyrekAltin() {
+        return ceyrekAltin;
+    }
+
+    public void setCeyrekAltin(String ceyrekAltin) {
+        this.ceyrekAltin = ceyrekAltin;
+    }
+
+    public String getYarimAltin() {
+        return yarimAltin;
+    }
+
+    public void setYarimAltin(String yarimAltin) {
+        this.yarimAltin = yarimAltin;
+    }
+
+    public String getTamAltin() {
+        return tamAltin;
+    }
+
+    public void setTamAltin(String tamAltin) {
+        this.tamAltin = tamAltin;
+    }
+
+    public String getCumhuriyetAltin() {
+        return cumhuriyetAltin;
+    }
+
+    public void setCumhuriyetAltin(String cumhuriyetAltin) {
+        this.cumhuriyetAltin = cumhuriyetAltin;
+    }
+}
